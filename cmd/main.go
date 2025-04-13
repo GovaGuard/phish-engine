280d9bd9a5c8653226a8dd20a9585e3d23781f9d
Nils Carstensen <me@nils-carstensen.de>
2025-04-13 11:59:38 +0200

    Added RethinkDB setup

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/holgerson97/phish-engine/internal/db"
 	"github.com/holgerson97/phish-engine/internal/router"
 )
 
 func main() {
+	log.Print("DB Connected")
+	_, err := db.New("localhost:28015")
+	if err != nil {
+		panic(err)
+	}
+
 	log.Print("Server Running")
 	if err := router.Run(); err != nil {
 		panic(err)
---
+++ internal/db/server.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"slices"
+
+	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
+)
+
+func New(url string) (*r.Session, error) {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: url,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := r.TableList().Run(session)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	var tables []string
+	if err := c.All(&tables); err != nil {
+		return nil, err
+	}
+
+	if !slices.Contains(tables, "Campaigns") {
+		_, err = r.TableCreate("Campaigns").Run(session)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return session, nil
+}
