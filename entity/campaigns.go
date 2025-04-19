acb7a8dca2e21ee1b96e79cf50569f3f738ed231
Nils Carstensen <me@nils-carstensen.de>
2025-04-19 16:37:12 +0200

    Added RethinkDB tags to campaigns struct

--- entity/campaigns.go
+++ entity/campaigns.go
@@ -1,7 +1,21 @@
 package entity
 
 type Campaign struct {
-	Name    string
-	State   string
-	OwnerID string
+	ID             string   `json:"id" rethinkdb:"id"`
+	CreatorID      string   `json:"creator_id" rethinkdb:"creator_id"`
+	OrganizationID string   `json:"organization_id" rethinkdb:"organization_id"`
+	Title          string   `json:"title" rethinkdb:"title"`
+	State          string   `json:"state" rethinkdb:"state"`
+	StartDate      string   `json:"start_date" rethinkdb:"start_date"`
+	EndDate        string   `json:"end_date" rethinkdb:"end_date"`
+	Targets        []Target `json:"targets" rethinkdb:"targets"`
+}
+
+type Target struct {
+	ID             string
+	OrganizationID string
+	EMail          string
+	FirstName      string
+	SurName        string
+	State          string
 }
