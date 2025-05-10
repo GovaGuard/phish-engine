2aa439199fb08449184d0b245c14485a979b73be
Nils Carstensen <me@nils-carstensen.de>
2025-05-10 15:19:50 +0200

    Added JSON and RethinkDB Validation Tags

--- entity/campaigns.go
+++ entity/campaigns.go
@@ -1,14 +1,17 @@
 package entity
 
+import "time"
+
 type Campaign struct {
-	ID             string   `json:"id" rethinkdb:"id"`
-	CreatorID      string   `json:"creator_id" rethinkdb:"creator_id"`
-	OrganizationID string   `json:"organization_id" rethinkdb:"organization_id"`
-	Title          string   `json:"title" rethinkdb:"title"`
-	State          string   `json:"state" rethinkdb:"state"`
-	StartDate      string   `json:"start_date" rethinkdb:"start_date"`
-	EndDate        string   `json:"end_date" rethinkdb:"end_date"`
-	Targets        []Target `json:"targets" rethinkdb:"targets"`
+	ID             string    `binding:"required" json:"id" rethinkdb:"id"`
+	CreatorID      string    `binding:"required" json:"creator_id" rethinkdb:"creator_id"`
+	OrganizationID string    `binding:"required" json:"organization_id" rethinkdb:"organization_id"`
+	Title          string    `binding:"required" json:"title" rethinkdb:"title"`
+	Status         string    `binding:"required" json:"status" rethinkdb:"status"`
+	StartDate      time.Time `binding:"required" json:"start_date" rethinkdb:"start_date"`
+	EndDate        time.Time `binding:"required" json:"end_date" rethinkdb:"end_date"`
+	SuccessRate    int16     `binding:"required" json:"success_rate" rethinkdb:"success_rate"`
+	Targets        []Target  `binding:"required" json:"targets" rethinkdb:"targets"`
 }
 
 type Target struct {
